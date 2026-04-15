04db3b96010497172b11bad186552bd9efd67c51
Dave Henton <dave@qlty.sh>
2026-04-15 15:44:57 -0500

    test: extend IssueRangeAdjusterTest with file-complexity, similar-code, identical-code, return-statements, and function-complexity success path

--- src/test/kotlin/com/qlty/intellij/IssueRangeAdjusterTest.kt
+++ src/test/kotlin/com/qlty/intellij/IssueRangeAdjusterTest.kt
@@ -44,6 +44,90 @@ class IssueRangeAdjusterTest : BasePlatformTestCase() {
         assertEquals(2, adjusted.endCol)
     }
 
+    fun testFileComplexityCollapseToStartLine() {
+        val document = EditorFactory.getInstance().createDocument("class Foo {\n  fun bar() {}\n}\n")
+        val issue = Issue(
+            tool = "qlty",
+            ruleKey = "file-complexity",
+            location = Location(range = Range(startLine = 1, endLine = 3)),
+        )
+
+        val adjusted = IssueRangeAdjuster.adjustRangeForSmells(issue, document)
+
+        assertEquals(0, adjusted.startLine)
+        assertEquals(0, adjusted.endLine)
+        assertEquals(0, adjusted.startCol)
+        assertEquals(0, adjusted.endCol)
+    }
+
+    fun testSimilarCodeCollapseToStartLine() {
+        val document = EditorFactory.getInstance().createDocument("val x = 1\nval y = 1\n")
+        val issue = Issue(
+            tool = "qlty",
+            ruleKey = "similar-code",
+            location = Location(range = Range(startLine = 1, endLine = 2)),
+        )
+
+        val adjusted = IssueRangeAdjuster.adjustRangeForSmells(issue, document)
+
+        assertEquals(0, adjusted.startLine)
+        assertEquals(0, adjusted.endLine)
+        assertEquals(0, adjusted.startCol)
+        assertEquals(0, adjusted.endCol)
+    }
+
+    fun testIdenticalCodeCollapseToStartLine() {
+        val document = EditorFactory.getInstance().createDocument("val x = 1\nval y = 1\n")
+        val issue = Issue(
+            tool = "qlty",
+            ruleKey = "identical-code",
+            location = Location(range = Range(startLine = 1, endLine = 2)),
+        )
+
+        val adjusted = IssueRangeAdjuster.adjustRangeForSmells(issue, document)
+
+        assertEquals(0, adjusted.startLine)
+        assertEquals(0, adjusted.endLine)
+        assertEquals(0, adjusted.startCol)
+        assertEquals(0, adjusted.endCol)
+    }
+
+    fun testReturnStatementsHighlightsFunctionName() {
+        val document = EditorFactory.getInstance().createDocument("fun calculate() = Unit\n")
+        val issue = Issue(
+            tool = "qlty",
+            ruleKey = "return-statements",
+            message = "Too many return statements in calculate",
+            snippet = "fun calculate() = Unit",
+            location = Location(range = Range(startLine = 1, endLine = 1)),
+        )
+
+        val adjusted = IssueRangeAdjuster.adjustRangeForSmells(issue, document)
+
+        assertEquals(0, adjusted.startLine)
+        assertEquals(0, adjusted.endLine)
+        assertEquals(4, adjusted.startCol)
+        assertEquals(13, adjusted.endCol)
+    }
+
+    fun testFunctionComplexityHighlightsFunctionNameWhenFound() {
+        val document = EditorFactory.getInstance().createDocument("fun doWork() = Unit\n")
+        val issue = Issue(
+            tool = "qlty",
+            ruleKey = "function-complexity",
+            message = "Function complexity for doWork",
+            snippet = "fun doWork() = Unit",
+            location = Location(range = Range(startLine = 1, endLine = 1)),
+        )
+
+        val adjusted = IssueRangeAdjuster.adjustRangeForSmells(issue, document)
+
+        assertEquals(0, adjusted.startLine)
+        assertEquals(0, adjusted.endLine)
+        assertEquals(4, adjusted.startCol)
+        assertEquals(10, adjusted.endCol)
+    }
+
     fun testFunctionComplexityFallsBackWhenFunctionNameIsMissing() {
         val document = EditorFactory.getInstance().createDocument("fun demo() = Unit\n")
         val issue = Issue(
